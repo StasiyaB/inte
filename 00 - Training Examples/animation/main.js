085857505597efd9cb35e4dc9ad018e68f73f139
StasiyaB <baryash.anastasiya@gmail.com>
2019-02-17 04:31:32 +0100

    Animation

---
+++ 00 - Training Examples/animation/main.js
@@ -0,0 +1,27 @@
+
+
+var canvas1 = new Canvas('canvas',"percent",  "RGBA(227,76,38,0.30)", '#e34c26', 90);
+var canvas2 = new Canvas('canvas2',"percent2", "RGBA(0,112,187,0.30)", '#0070BB', 85);
+var canvas3 = new Canvas('canvas3',"percent3", "RGBA(240,219,79,0.30)", '#f0db4f', 40);
+var canvas4 = new Canvas('canvas4',"percent4", "RGBA(137,147,190,0.30)", '#8993be', 60);
+var canvas5 = new Canvas('canvas5',"percent5", "RGBA(0,117,143,0.30)", '#00758F', 75);
+console.log(canvas1);
+
+canvas1.arcMove();
+canvas2.arcMove();
+canvas3.arcMove();
+canvas4.arcMove();
+canvas5.arcMove();
+
+/*var text1 = new TextShower (3, 'thechnology', 'HTML');
+var text2 = new TextShower (4, 'thechnology1', 'CSS');
+var text3 = new TextShower (5, 'thechnology2', 'JS');
+var text4 = new TextShower (6, 'thechnology3', 'PHP');
+var text5 = new TextShower (7, 'thechnology4', 'MySQL');
+console.log(text1);
+text1.showText();
+text2.showText();
+text3.showText();
+text4.showText();
+text5.showText();
+*/
